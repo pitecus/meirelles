f6e10451e5c1fbd4cac7abd9dbc6985daf9343d1
Leonardo Meirelles <pitecus@gmail.com>
2018-01-06 12:43:32 -0500

    Adding ng-bootstrap and bootstrap dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -1,33 +1,72 @@
+const karmaJasmine = require('karma-jasmine');
+const karmaChromeLauncher = require('karma-chrome-launcher');
+const karmaJasmineHtmlReporter = require('karma-jasmine-html-reporter');
+const karmaCoverageIstanbulReporter = require('karma-coverage-istanbul-reporter');
+const karmaAngularCli = require('@angular/cli/plugins/karma');
+
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular/cli'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
-    ],
-    client:{
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
-      fixWebpackSourcePaths: true
-    },
     angularCli: {
       environment: 'dev'
     },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
+
+    // Enable or disable watching files and executing the tests whenever one of these files changes.
     autoWatch: true,
-    browsers: ['Chrome'],
+
+    // The root path location that will be used to resolve all relative paths defined in files and exclude.
+    basePath: '',
+
+    // A list of browsers to launch and capture.
+    browsers: [
+      'Chrome'
+    ],
+
+    client: {
+      // Karma clears the context window upon the completion of running the tests.
+      clearContext: false
+    },
+
+    // Enable or disable colors in the output (reporters and logs).
+    colors: true,
+    coverageIstanbulReporter: {
+      fixWebpackSourcePaths: true,
+      reports: [
+        'html',
+        'lcovonly'
+      ]
+    },
+
+    // List of test frameworks you want to use.
+    frameworks: [
+      'jasmine',
+      '@angular/cli'
+    ],
+
+    // Level of logging.
+    logLevel: config.LOG_INFO,
+
+    // List of plugins to load.
+    plugins: [
+      karmaJasmine,
+      karmaChromeLauncher,
+      karmaJasmineHtmlReporter,
+      karmaCoverageIstanbulReporter,
+      karmaAngularCli
+    ],
+
+    // The port where the web server will be listening.
+    port: 9876,
+
+    // A list of reporters to use.
+    reporters: [
+      'progress',
+      'kjhtml'
+    ],
+
+    // Continuous Integration mode.
     singleRun: false
   });
 };
