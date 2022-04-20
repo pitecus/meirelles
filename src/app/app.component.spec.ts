4bbe798e6cc029fe95a63df03876163ff21687ec
Leo Meirelles <leo.meirelles@adp.com>
2022-04-20 11:46:48 -0400

    test: fix tests dependency

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TestBed } from '@angular/core/testing';
+import { IndexNavbarComponent } from './components/navbars/index-navbar/index-navbar.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -11,8 +12,15 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        IndexNavbarComponent
       ],
+      providers: [
+        {
+          provide: Window,
+          useValue: window
+        }
+      ]
     }).compileComponents();
   });
 
