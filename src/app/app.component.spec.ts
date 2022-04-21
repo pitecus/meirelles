20bfbe1f9843a3918b0348915d5b066e0156248e
Leo Meirelles <leo.meirelles@adp.com>
2022-04-21 13:49:22 -0400

    chore: remove unused code for navigation

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,4 @@
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TestBed } from '@angular/core/testing';
 import { IndexNavbarComponent } from './components/navbars/index-navbar/index-navbar.component';
@@ -8,7 +7,6 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        BrowserAnimationsModule,
         RouterTestingModule
       ],
       declarations: [
@@ -33,53 +31,5 @@ describe('AppComponent', () => {
 
     // Then it should be initialized.
     expect(app).toBeTruthy();
-  });
-
-  it('should have initial values', () => {
-    // Given a component.
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-
-    // Then it should the default values.
-    expect(app.navigation)
-      .toEqual([
-        {
-          label: 'resume',
-          link: '/resume'
-        },
-        {
-          label: 'changelog',
-          link: '/changelog'
-        }
-      ]);
-    expect(app.menuState)
-      .toBe('open');
-  });
-
-  it('should open the menu', () => {
-    // Given a component.
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-
-    // When click to open the menu.
-    app.toggleMenu();
-
-    // Then it update the values.
-    expect(app.menuState)
-      .toBe('closed');
-  });
-
-  it('should open and close the menu', () => {
-    // Given a component.
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-
-    // When click to open and close the menu.
-    app.toggleMenu();
-    app.toggleMenu();
-
-    // Then it update the values.
-    expect(app.menuState)
-      .toBe('open');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,77 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component } from '@angular/core';
 
 @Component({
-  animations: [
-    trigger('openClose', [
-      state('open', style({
-
-      })),
-      state('closed', style({
-        transform: 'translateX(-100%)'
-      })),
-      transition('open => closed', [
-        animate('350ms')
-      ]),
-      transition('closed => open', [
-        animate('350ms')
-      ])
-    ])
-  ],
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit {
-  public navbarOpen = false;
-
-  /**
-   * The navigation items.
-   */
-  public readonly navigation = [
-    {
-      label: 'resume',
-      link: '/resume'
-    },
-    {
-      label: 'changelog',
-      link: '/changelog'
-    }
-  ];
-
-  /**
-   * Valid menu states.
-   */
-  public menuState: 'open' | 'closed' = 'open';
-
-  public ngOnInit() {
-    setTimeout(() => {
-      this.toggleMenu();
-    }, 1000);
-  }
-
-  /**
-   * Toggle the menu
-   */
-  public toggleMenu(): void {
-    // Close menu.
-    if (this.menuState === 'open') {
-      this.menuState = 'closed';
-      return;
-    }
-
-    // Open menu.
-    this.menuState = 'open';
-  }
-
-  /**
-   * Close the menu
-   */
-  public closeNav(): void {
-    // Open menu.
-    this.menuState = 'closed';
-  }
-
-  setNavbarOpen() {
-    this.navbarOpen = !this.navbarOpen;
-  }
+export class AppComponent {
 }
